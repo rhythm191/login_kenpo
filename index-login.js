de58b54cc3639129862ec7fc3991ef8c52b0e47a
kimpara <keigo.kimpara@gi-no.jp>
2018-10-13 21:01:04 +0900

    feat: split login and setting command

---
+++ index-login.js
@@ -0,0 +1,39 @@
+// auto login kanpo(my health web)
+'use strict';
+
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+const path = require("path");
+
+const userHome = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+
+fs.access(path.join(userHome, '.login_kanpo_env.json'), function (err) {
+  if (err) {
+    console.log('execute `login_kanpo setting`')
+    process.exit();
+  }
+});
+
+var setting = require(path.join(userHome, '.login_kanpo_env.json'));
+
+const login_url = 'https://its-kenpo.mhweb.jp/';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(login_url);
+
+  await page.type('#UserKigou', setting.kigo);
+  await page.type('#UserBangou', setting.bango);
+  await page.type('#UserPassword', setting.password);
+
+  await page.evaluate(({}) => {
+    $('#UserIndexForm').submit();
+  },{});
+
+  await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
+
+  console.log('login success');
+
+  await browser.close();
+})();
--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-// auto login kanpo(my health web)
-const puppeteer = require('puppeteer');
+#!/usr/bin/env node
+'use strict';
 
-const login_url = 'https://its-kenpo.mhweb.jp/';
+var program = require('commander');
 
-if (process.argv.length != 5) {
-  console.log('USAGE: node index.js kigo bango password');
-  return -1
-}
-
-const kigo = process.argv[2];
-const bango = process.argv[3];
-const password = process.argv[4];
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(login_url);
-
-  await page.type('#UserKigou', kigo);
-  await page.type('#UserBangou', bango);
-  await page.type('#UserPassword', password);
-
-  await page.evaluate(({}) => {
-    $('#UserIndexForm').submit();
-  },{});
-
-  await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
-
-  console.log('login success');
-
-  await browser.close();
-})();
+program
+  .version('1.0.0')
+  .command('setting', 'setting login info')
+  .command('login', 'login my health web', {isDefault: true})
+  .parse(process.argv);
