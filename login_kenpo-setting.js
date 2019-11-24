6e85a819fd189eded285985cbe819db22b136f8f
kimpara <keigo.kimpara@gi-no.jp>
2019-11-24 10:34:43 +0900

    feat: rename package name

---
+++ login_kenpo-setting.js
@@ -0,0 +1,34 @@
+// setting kenpo(my health web)
+"use strict";
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const path = require("path");
+const userHome =
+  process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+
+const questions = [
+  {
+    type: "input",
+    name: "kigo",
+    message: "kigo:"
+  },
+  {
+    type: "input",
+    name: "bango",
+    message: "bango:"
+  },
+  {
+    type: "password",
+    name: "password",
+    message: "password:"
+  }
+];
+
+inquirer.prompt(questions).then(function(answer) {
+  fs.writeFileSync(
+    path.join(userHome, ".login_kenpo_env.json"),
+    JSON.stringify(answer)
+  );
+});
